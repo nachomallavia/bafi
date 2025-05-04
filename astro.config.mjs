6f5117b9b51a2c4c7d1e1304ba1fae62784081b9
nachomallavia <nachomallavia@hotmail.com>
2025-05-04 15:21:09 -0300

    ajuste posicion titulos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
+    adapter: vercel({}),
 });
