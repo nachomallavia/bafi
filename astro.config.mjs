c6e74522f42cf4047d0344a97e4ef4fc69165983
nachomallavia <nachomallavia@hotmail.com>
2025-05-17 15:48:05 -0300

    Avances e iconos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), icon()],
     adapter: vercel({}),
 });
